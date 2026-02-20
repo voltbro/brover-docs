c1b7e8279055c7e1a524d689a48b2ef55e7772be
NikolayIvanovWS <kolyan_ivanov@list.ru>
2026-02-20 16:01:45 +0300

    edit: docusaurus.config.js delete: some files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   url: 'https://docs.vbcores.ru',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/brover/',
+  baseUrl: '/brover-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
